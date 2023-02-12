d9f3f47451a6f71c28759a38ff427274550e36cc
Andresfgc <afgarciacano@gmail.com>
2023-02-12 15:24:34 +0000

    Add board and mines

--- run.py
+++ run.py
@@ -1,1 +1,50 @@
-#initial commit
+import random
+
+#Board player can not see
+board =[[0,0,0,0,0],   #0= no mine
+        [0,0,0,0,0],   #1= mine
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0]]
+
+
+#Board player can see
+boardVisible = [[-1,-1,-1,-1,-1],  #-1=unkown
+                [-1,-1,-1,-1,-1],
+                [-1,-1,-1,-1,-1],
+                [-1,-1,-1,-1,-1],
+                [-1,-1,-1,-1,-1]]
+
+#add mines
+numMines = 7
+
+
+num = 0 #num mines
+while num < numMines:
+    row=random.randint(0, 4)
+    col=random.randint(0, 4)
+    if board[row][col] == 0:
+        board[row][col]= 1 #add mine
+        num = num+1
+
+
+def displayBoard():
+    for row in range(0,5):
+        for col in range(0,5):
+            print(board[row][col], end=" ")
+        print("")
+
+def displayBoardVisible():
+    print("-"*21)
+    for row in range(0,5):
+        print("| ", end="")
+        for col in range(0,5):
+            if boardVisible[row][col] == -1:
+                print(" ", end= " | ")
+            else:
+                print(boardVisible[row][col], end=" | ")
+        print("")
+        print("-"*21)
+
+displayBoard()
+displayBoardVisible()
