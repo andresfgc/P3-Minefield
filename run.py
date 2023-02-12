6e64b94395c7d0564ef9c39f87aac7fe05a64814
Andresfgc <afgarciacano@gmail.com>
2023-02-12 15:42:03 +0000

    Add movement input for row & col coordinates

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import random
 
+"""
+Create a board for mines and for the player
+"""
+
 #Board player can not see
 board =[[0,0,0,0,0],   #0= no mine
         [0,0,0,0,0],   #1= mine
@@ -17,8 +21,6 @@ boardVisible = [[-1,-1,-1,-1,-1],  #-1=unkown
 
 #add mines
 numMines = 7
-
-
 num = 0 #num mines
 while num < numMines:
     row=random.randint(0, 4)
@@ -27,13 +29,17 @@ while num < numMines:
         board[row][col]= 1 #add mine
         num = num+1
 
-
+"""
+Essential for testing if the mines were added correctly.
+It will be displayed when player steps on a mine.
+"""
 def displayBoard():
     for row in range(0,5):
         for col in range(0,5):
             print(board[row][col], end=" ")
         print("")
 
+#Will show the board to Player without revealing mines´s locations
 def displayBoardVisible():
     print("-"*21)
     for row in range(0,5):
@@ -48,3 +54,14 @@ def displayBoardVisible():
 
 displayBoard()
 displayBoardVisible()
+
+movement=0
+while movement < (25 - numMines):
+    row= int(input("Select a row(1-5): "))
+    col= int(input("Select a col(1-5): "))
+    if board[row][col] == 1:
+        print("Ooops!!! You stepped on a mine.")
+        displayBoard()
+    else:
+        boardVisible[row][col] = checkMinesAround(row, col)
+        displayBoard()
